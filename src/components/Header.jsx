bafd18857e8403fc0549149c511259b3798053da
Tommy Johnsen <7oiden@gmail.com>
2024-01-22 14:21:01 +0100

    mobile menu added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import { Link, NavLink } from "react-router-dom";
+import { MdMenu, MdClose } from "react-icons/md";
+import { useState } from "react";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function handleToggle() {
+    setIsMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen);
+  }
+
+  const menuIcon = isMenuOpen ? <MdClose /> : <MdMenu />;
+
   return (
     <header className="header">
       <div className="header__container">
-        <nav>
+        <nav className="desktop">
           <Link to="/">#</Link>
           <div className="nav__link-container">
             <NavLink
@@ -31,6 +41,20 @@ export default function Header() {
             >
               About
             </NavLink>
+          </div>
+        </nav>
+        <nav className={isMenuOpen ? "mobile" : null}>
+          <div>
+            <div className="menu-icon" onClick={handleToggle}>
+              {menuIcon}
+            </div>
+            {isMenuOpen && (
+              <div className="mobile__link-container">
+                <NavLink to="projects">Projects</NavLink>
+                <NavLink to="background">Background</NavLink>
+                <NavLink to="about">About</NavLink>
+              </div>
+            )}
           </div>
         </nav>
       </div>
