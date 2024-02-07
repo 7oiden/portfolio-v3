b4fcaba27baf181241b668f04ed2cd3de8b96ef5
Tommy Johnsen <7oiden@gmail.com>
2024-02-07 12:13:03 +0100

    fade bg improved

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import Navbar from "../components/navigation/Navbar";
 import NavDropdown from "../components/navigation/NavDropdown";
+import { useSpring, animated } from "@react-spring/web";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const fadeBg = useSpring({
+    backgroundColor: isMenuOpen ? "rgba(0, 0, 0, 0.2)" : "rgba(0, 0, 0, 0)",
+    backdropFilter: isMenuOpen ? "blur(10px)" : "blur(0px)",
+    display: isMenuOpen ? "block" : "none",
+    config: { duration: 200 },
+  });
 
   function handleToggle() {
     setIsMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen);
@@ -11,7 +19,7 @@ export default function Header() {
 
   return (
     <>
-      <div className={isMenuOpen ? "fade-bg" : ""}></div>
+      <animated.div className="fade-bg" style={fadeBg}></animated.div>
       <header className="header">
         <Navbar handleToggle={handleToggle} isMenuOpen={isMenuOpen} />
         <NavDropdown isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
